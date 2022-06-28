689936e715eaacc3baa6e5d1cba13c080291125d
Gord.Tulloch <gord.tulloch@gmail.com>
2022-06-28 15:42:10 -0500

    Revised SQL

--- database.sql
+++ database.sql
@@ -1,64 +1,146 @@
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 28, 2022 at 03:40 PM
+-- Server version: 8.0.29-0ubuntu0.22.04.2
+-- PHP Version: 8.1.2
 
--- 
--- Table structure for table `categories`
--- 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `categories` (
-  `id` smallint(6) NOT NULL,
-  `name` varchar(256) NOT NULL,
-  `description` text NOT NULL,
-  `position` smallint(6) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- 
--- Table structure for table `pm`
--- 
-
-CREATE TABLE `pm` (
-  `id` bigint(20) NOT NULL,
-  `id2` int(11) NOT NULL,
-  `title` varchar(256) NOT NULL,
-  `user1` bigint(20) NOT NULL,
-  `user2` bigint(20) NOT NULL,
-  `message` text NOT NULL,
-  `timestamp` int(10) NOT NULL,
-  `user1read` varchar(3) NOT NULL,
-  `user2read` varchar(3) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+--
+-- Database: `obsy`
+--
+CREATE DATABASE IF NOT EXISTS `obsy` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `obsy`;
 
 -- --------------------------------------------------------
 
--- 
--- Table structure for table `topics`
--- 
+--
+-- Table structure for table `instrument`
+--
 
-CREATE TABLE `topics` (
-  `parent` smallint(6) NOT NULL,
-  `id` int(11) NOT NULL,
-  `id2` int(11) NOT NULL,
-  `title` varchar(256) NOT NULL,
-  `message` longtext NOT NULL,
-  `authorid` int(11) NOT NULL,
-  `timestamp` int(11) NOT NULL,
-  `timestamp2` int(11) NOT NULL,
-  PRIMARY KEY  (`id`,`id2`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `instrument` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `observatory` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `mission` varchar(255) DEFAULT NULL,
+  `defaultPerson` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
--- 
+--
+-- Table structure for table `observatory`
+--
+
+CREATE TABLE `observatory` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `tz` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `defaultObs` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `person`
+--
+
+CREATE TABLE `person` (
+  `id` int NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `tz` varchar(255) NOT NULL,
+  `defaultPerson` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
--- 
+--
 
 CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `avatar` text NOT NULL,
-  `signup_date` int(10) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `signup_date` int NOT NULL,
+  `security_level` int NOT NULL,
+  `tzone` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `signup_date`, `security_level`, `tzone`) VALUES
+(1, 'gord.tulloch@gmail.com', '9231cac17f8018e10b02df69f4e09ab426a1e59e', 'gord.tulloch@gmail.com', 1655487073, 99, 'America/Chicago');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `instrument`
+--
+ALTER TABLE `instrument`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `observatory`
+--
+ALTER TABLE `observatory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `person`
+--
+ALTER TABLE `person`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `instrument`
+--
+ALTER TABLE `instrument`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `observatory`
+--
+ALTER TABLE `observatory`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `person`
+--
+ALTER TABLE `person`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
